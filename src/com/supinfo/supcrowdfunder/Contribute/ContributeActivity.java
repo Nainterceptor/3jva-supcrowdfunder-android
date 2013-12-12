22eafaf02c43011de4b8eafdbc4fb014e999927b
Rodrive <robin.lebert@gmail.com>
2013-12-12 12:38:58 +0100

    Création activity et layout pour le contribute Refs #264

---
+++ src/com/supinfo/supcrowdfunder/Contribute/ContributeActivity.java
@@ -0,0 +1,15 @@
+package com.supinfo.supcrowdfunder.Contribute;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.supinfo.supcrowdfunder.R;
+
+/**
+ * Created by Robin on 12/12/13.
+ */
+public class ContributeActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contribute_activity);
+    }
+}
