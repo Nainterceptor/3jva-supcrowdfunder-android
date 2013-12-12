d1b797638b9d2ea12c194bd4ffb8d4348dd83610
Rodrive <robin.lebert@gmail.com>
2013-12-12 18:26:27 +0100

    Contribute terminé à 80%, tout l'évènementiel est géré. Il reste a communiquer avec l'api et se mettre d'accord sur le design refs #264

--- src/com/supinfo/supcrowdfunder/Contribute/ContributeActivity.java
+++ src/com/supinfo/supcrowdfunder/Contribute/ContributeActivity.java
@@ -1,22 +1,63 @@
 package com.supinfo.supcrowdfunder.Contribute;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.supinfo.supcrowdfunder.R;
 
 /**
  * Created by Robin on 12/12/13.
  */
 public class ContributeActivity extends Activity {
+    Resources res = null;
+    Button contributeButton = null;
+    TextView body = null;
+    EditText amount = null;
+    AlertDialog.Builder alert = null;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contribute_activity);
 
-        Resources res = getResources();
-        TextView title = (TextView) findViewById(R.id.thanks);
+        res = getResources();
+        body = (TextView) findViewById(R.id.thanks);
+        amount = (EditText) findViewById(R.id.amount);
+        contributeButton = (Button) findViewById(R.id.button);
+        alert = new AlertDialog.Builder(this);
 
-        title.setText(res.getString(R.string.contributeThanks1)+" "+"project.name"+". \n\n"+"project.user.firstname"+" "+"project.user.lastname"+res.getString(R.string.contributeThanks2));
+        body.setText(res.getString(R.string.contributeThanks1)+" "+"project.name"+". \n\n"+"project.user.firstname"+" "+"project.user.lastname"+res.getString(R.string.contributeThanks2));
+        contributeButton.setOnClickListener(contributeListener);
     }
+    private View.OnClickListener contributeListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            alert.setMessage(res.getString(R.string.contributeAlertContent)+" "+amount.getText().toString()+"€");
+            alert.setTitle(res.getString(R.string.contributeAlertTitle));
+            alert.setPositiveButton(res.getString(R.string.validate), alertValidateListener);
+            alert.setNegativeButton(res.getString(R.string.cancel), alertCancelListener);
+            alert.show();
+        }
+    };
+
+    private DialogInterface.OnClickListener alertValidateListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            Toast.makeText(ContributeActivity.this,res.getString(R.string.contributeToastValidate),Toast.LENGTH_SHORT).show();
+        }
+    };
+    private DialogInterface.OnClickListener alertCancelListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            Toast.makeText(ContributeActivity.this,res.getString(R.string.contributeToastCancel),Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
 }
