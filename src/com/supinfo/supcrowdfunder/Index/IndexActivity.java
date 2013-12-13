12a242712ba9343728bae553218e15003bdddc33
Fireaxe <axess9fireaxe@gmail.com>
2013-12-13 21:54:09 +0100

    Index : Titre + listView

--- src/com/supinfo/supcrowdfunder/Index/IndexActivity.java
+++ src/com/supinfo/supcrowdfunder/Index/IndexActivity.java
@@ -2,9 +2,12 @@ package com.supinfo.supcrowdfunder.Index;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
+import com.supinfo.supcrowdfunder.Project.ProjectActivity;
 import com.supinfo.supcrowdfunder.R;
 
 import java.util.ArrayList;
@@ -13,7 +16,7 @@ import java.util.ArrayList;
  * Created by Fireaxe on 13/12/13.
  */
 public class IndexActivity extends Activity {
-    ListView projectsList =null;
+    ListView projectsList = null;
     AlertDialog.Builder alert = null;
     Resources res = null;
 
@@ -27,7 +30,15 @@ public class IndexActivity extends Activity {
         projectsList = (ListView) findViewById(R.id.projectsList);
 
         ArrayList<String> projects = new ArrayList<String>();
-        projects.add("test");
+        projects.add("project_name");
         projectsList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, projects));
+        projectsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(IndexActivity.this, ProjectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ src/com/supinfo/supcrowdfunder/Project/ProjectActivity.java
@@ -0,0 +1,16 @@
+package com.supinfo.supcrowdfunder.Project;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.supinfo.supcrowdfunder.R;
+
+/**
+ * Created by Fireaxe on 13/12/13.
+ */
+public class ProjectActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.project_details_activity);
+    }
+}
