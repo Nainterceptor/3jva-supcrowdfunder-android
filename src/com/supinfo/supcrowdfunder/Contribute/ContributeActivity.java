de9d34c4191a014c56618ded5a2aa375a541cf39
Rodrive <robin.lebert@gmail.com>
2013-12-13 01:03:37 +0100

    Gestion des erreurs sur le contribute ajouté. Ajout d'un thème général sur tout le projet (device >API 11, sinon thème par défaut)

--- src/com/supinfo/supcrowdfunder/Contribute/ContributeActivity.java
+++ src/com/supinfo/supcrowdfunder/Contribute/ContributeActivity.java
@@ -32,17 +32,28 @@ public class ContributeActivity extends Activity {
         contributeButton = (Button) findViewById(R.id.button);
         alert = new AlertDialog.Builder(this);
 
-        body.setText(res.getString(R.string.contributeThanks1)+" "+"project.name"+". \n\n"+"project.user.firstname"+" "+"project.user.lastname"+res.getString(R.string.contributeThanks2));
+        body.setText(res.getString(R.string.contributeThanks1) + " " + "project.name" + ". \n\n" +
+                "project.user.firstname" + " " + "project.user.lastname" +
+                res.getString(R.string.contributeThanks2));
         contributeButton.setOnClickListener(contributeListener);
     }
     private View.OnClickListener contributeListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            alert.setMessage(res.getString(R.string.contributeAlertContent)+" "+amount.getText().toString()+"€");
-            alert.setTitle(res.getString(R.string.contributeAlertTitle));
-            alert.setPositiveButton(res.getString(R.string.validate), alertValidateListener);
-            alert.setNegativeButton(res.getString(R.string.cancel), alertCancelListener);
-            alert.show();
+            if (amount.getText().length() == 0){
+                Toast.makeText(ContributeActivity.this,res.getString(R.string.contributeToastEmpty),Toast.LENGTH_SHORT).show();
+            }
+            else if (Long.parseLong(amount.getText().toString()) <= 0L){
+                Toast.makeText(ContributeActivity.this,res.getString(R.string.contributeToastInvalid),Toast.LENGTH_SHORT).show();
+                amount.getText().clear();
+            }
+            else{
+                alert.setMessage(res.getString(R.string.contributeAlertContent)+" "+amount.getText().toString()+"€");
+                alert.setTitle(res.getString(R.string.contributeAlertTitle));
+                alert.setPositiveButton(res.getString(R.string.validate), alertValidateListener);
+                alert.setNegativeButton(res.getString(R.string.cancel), alertCancelListener);
+                alert.show();
+            }
         }
     };
 
